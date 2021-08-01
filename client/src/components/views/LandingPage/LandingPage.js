a47c3aecd100cabd5675f82d92f1b9954563b7e0
momo <wdudtlrw78@gmail.com>
2021-08-01 17:30:12 +0900

    Add: 더보기 버튼 만들기

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -4,17 +4,49 @@ import { Icon, Col, Card, Row } from 'antd';
 import Meta from 'antd/lib/card/Meta';
 import ImageSlider from '../../utils/ImageSlider';
 
-const LandingPage = () => {
+function LandingPage() {
   const [Products, setProducts] = useState([]);
+  const [Skip, setSkip] = useState(0);
+  const [Limit, setLimit] = useState(6);
+  const [PostSize, setPostSize] = useState(0);
+
   useEffect(() => {
-    Axios.get('/api/product/products').then((response) => {
+    let body = {
+      skip: Skip,
+      limit: Limit,
+    };
+
+    getProducts(body);
+  }, []);
+
+  const getProducts = (body) => {
+    Axios.post('/api/product/products', body).then((response) => {
       if (response.data.success) {
-        setProducts(response.data.productInfo);
+        if (body.loadMore) {
+          setProducts([...Products, ...response.data.productInfo]);
+        } else {
+          setProducts(response.data.productInfo);
+        }
+        setPostSize(response.data.postSize);
       } else {
         alert('상품들을 가져오는데 실패했습니다.');
       }
     });
-  }, []);
+  };
+
+  const loadmoreHandler = () => {
+    let skip = Skip + Limit;
+
+    let body = {
+      skip: skip,
+      limit: Limit,
+      loadMore: true,
+    };
+
+    getProducts(body);
+
+    setSkip(skip);
+  };
 
   const renderCards = Products.map((product, index) => {
     console.log(product);
@@ -27,6 +59,7 @@ const LandingPage = () => {
       </Col>
     );
   });
+
   return (
     <div style={{ width: '75%', margin: '3rem auto ' }}>
       <div style={{ textAlign: 'center' }}>
@@ -42,11 +75,15 @@ const LandingPage = () => {
       {/* Cards */}
       <Row gutter={[16, 16]}>{renderCards}</Row>
 
-      <div style={{ justifyContent: 'center' }}>
-        <button>더보기</button>
-      </div>
+      <br />
+
+      {PostSize >= Limit && (
+        <div style={{ justifyContent: 'center' }}>
+          <button onClick={loadmoreHandler}>더보기</button>
+        </div>
+      )}
     </div>
   );
-};
+}
 
 export default LandingPage;
