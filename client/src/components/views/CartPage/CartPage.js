6e08ed34794ae0b7a924b62b377e0c0ab2dc3e06
momo <wdudtlrw78@gmail.com>
2021-08-03 20:01:26 +0900

    Add: 카트에 들어있는 상품들 가격 계산

--- client/src/components/views/CartPage/CartPage.js
+++ client/src/components/views/CartPage/CartPage.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { getCartItems } from '../../../_actions/user_actions';
 import UserCardBlock from './Sections/UserCardBlock';
@@ -6,6 +6,8 @@ import UserCardBlock from './Sections/UserCardBlock';
 function CartPage() {
   const dispatch = useDispatch();
   const user = useSelector((state) => state.user);
+
+  const [Total, setTotal] = useState(0);
 
   useEffect(() => {
     let cartItems = [];
@@ -17,15 +19,34 @@ function CartPage() {
           cartItems.push(item.id);
         });
 
-        dispatch(getCartItems(cartItems, user.userData.cart));
+        dispatch(getCartItems(cartItems, user.userData.cart)).then(
+          (response) => {
+            calculateTotal(response.payload);
+          }
+        );
       }
     }
   }, [user.userData]);
+
+  let calculateTotal = (cartDetail) => {
+    let total = 0;
+
+    cartDetail.map((item) => {
+      total += parseInt(item.price, 10) * item.quantity;
+    });
+
+    setTotal(total);
+  };
+
   return (
     <div style={{ width: '85%', margin: '3rem auto' }}>
       <h1>My Cart</h1>
       <div>
         <UserCardBlock products={user.cartDetail} />
+      </div>
+
+      <div style={{ marginTop: '3rem' }}>
+        <h2>Total Amount: ${Total}</h2>
       </div>
     </div>
   );
